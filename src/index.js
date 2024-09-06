06e48703cc2c9e132061b3f9f6d1862f72cc9d35
Le Cong Nguyen <lenguyentime16@gmail.com>
2024-09-06 17:40:30 +0700

    custom logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import app from './app.js'
+import logger from './configs/logger.config.js';
 
 
 
 
 //env varialbles
 const PORT = process.env.PORT || 8000
-console.log(process.env.NODE_ENV);
+
+
+
 app.listen(PORT, () => {
-    console.log(`Server is listening at ${PORT}....`);
+    logger.info(`Server is listening at ${PORT}....`);
 });
