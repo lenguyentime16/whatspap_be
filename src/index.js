c053e45d99d3a8907d4104d100746bc3807da437
Le Cong Nguyen <lenguyentime16@gmail.com>
2024-09-08 22:28:03 +0700

    connect MongoDB

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 import app from './app.js'
 import logger from './configs/logger.config.js';
-
+import mongoose from 'mongoose';
 
 
 
 //env varialbles
 const PORT = process.env.PORT || 8000
+const {DATABASE_URL} = process.env
 
+//exit on mongoDB error
+mongoose.connection.on('error', (err) => {
+    logger.error(`MongoDB connection error: ${err}`)
+    process.exit(1);
+})
 
+// mongodb debug mode
+if(process.env.NODE_ENV !== "production" ) {
+    mongoose.set("debug", true)
+}
+
+//mongoDB connection
+mongoose.connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    logger.info('Connected to MongoDB')
+});
 
 let server = app.listen(PORT, () => {
-    logger.info(`Server is listening at ${PORT}....`);
+    logger.info(`Server is listening at ${PORT}.`);
     console.log("process id", process.pid);
 });
 
