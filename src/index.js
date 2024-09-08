dc5a6d61fa46193da674c9489b49c521cb00726d
Le Cong Nguyen <lenguyentime16@gmail.com>
2024-09-08 20:46:20 +0700

    setup route , trim request, add controller

--- src/index.js
+++ src/index.js
@@ -9,6 +9,36 @@ const PORT = process.env.PORT || 8000
 
 
 
-app.listen(PORT, () => {
+let server = app.listen(PORT, () => {
     logger.info(`Server is listening at ${PORT}....`);
+    console.log("process id", process.pid);
 });
+
+
+//handle server errors
+
+const exitHandler = () => {
+    if (server) {
+        logger.info('Server closed');
+        process.exit(1);
+    }
+    else {
+        process.exit(1);
+    }
+}
+
+
+const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    exitHandler();
+}
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+//SIGTERM
+process.on("SIGTERM", () => {
+    if (server) {
+        logger.info('Server closed');
+        process.exit(1);
+    }
+})
